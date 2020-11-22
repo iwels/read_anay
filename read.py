78e749b3ef4ec98bf8f4c866e337b435ed1c5cdf
Ling Chao <iwelshaha@gmail.com>
2020-11-22 07:44:08 +0100

    calc average word

--- read.py
+++ read.py
@@ -3,8 +3,14 @@ count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-		count +=1
-		if count % 1000 == 0:
+		count += 1
+		if count % 10000 == 0:
 			print(len(data))
 
-print('完成讀取, 共有', len(data), '筆資料')
+print('讀取完成，共有', len(data), '筆資料')
+
+sum_length = 0
+for d in data:
+	sum_length = sum_length + len(d)
+
+print('每一筆的平均長度為', sum_length/len(data), '個字')
